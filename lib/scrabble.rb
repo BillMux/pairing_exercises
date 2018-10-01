7935f30dbd6a9f27cc0ad82de23a0eb2fbce4186
Henry Cooksley <henry.cooksley@gmail.com>
2018-10-01 16:49:18 +0100

    use ints as hash keys

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -5,26 +5,25 @@ class Scrabble
 
   def score
     total_score = 0
-    
-    scores_hash = {}
-    scores_hash["1"]  = ['a','e','i','o','u','l','n','r','s','t']
-    scores_hash["2"]  = ['d','g']
-    scores_hash['3']  = ['b','c','m','p']
-    scores_hash['4']  = ['f','h','v','w','y']
-    scores_hash['5']  = ['k']
-    scores_hash['8']  = ['j','x']
-    scores_hash['10'] = ['q','z']
+
+    scores_hash = {
+      1 => ['a','e','i','o','u','l','n','r','s','t'],
+      2 => ['d','g'],
+      3 => ['b','c','m','p'],
+      4 => ['f','h','v','w','y'],
+      5 => ['k'],
+      8 => ['j','x'],
+      10 => ['q','z'],
+    }
 
     if @word == nil
       return 0
     end
 
-    letters = @word.chars
-
-    letters.each do |l|
+    @word.chars.each do |l|
       scores_hash.each do |score, tile_array|
         if tile_array.include? l.downcase
-          total_score += score.to_i
+          total_score += score
         end
       end
     end
